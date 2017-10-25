d9b29d8f3c964ae6dee117323eccc47bf47ada3e
tcgwl <tcgwl_123@126.com>
2017-10-25 20:20:02 +0800

    chapter10-downloadservicedemo初次提交，完成DownloadTask。

---
+++ chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/interfaces/DownloadListener.java
@@ -0,0 +1,8 @@
+package com.chapter10.downloadservice.demo.interfaces;
+
+/**
+ * Created by Archer on 2017/10/25.
+ */
+
+public interface DownloadListener {
+}
---
+++ chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/utils/DownloadTask.java
@@ -0,0 +1,8 @@
+package com.chapter10.downloadservice.demo.utils;
+
+/**
+ * Created by Archer on 2017/10/25.
+ */
+
+public class DownloadTask {
+}
