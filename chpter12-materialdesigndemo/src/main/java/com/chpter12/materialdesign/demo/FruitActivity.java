5bdd1240639b97ad50cb3fca8f62285925ee789a
tcgwl <tcgwl_123@126.com>
2017-10-25 16:17:29 +0800

    CollapsingToolbar,NestedScrollView的使用，实现可折叠式标题栏。

---
+++ chpter12-materialdesigndemo/src/main/java/com/chpter12/materialdesign/demo/FruitActivity.java
@@ -0,0 +1,57 @@
+package com.chpter12.materialdesign.demo;
+
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class FruitActivity extends AppCompatActivity {
+    public static final String FRUIT_NAME = "fruit_name";
+    public static final String FRUIT_IMAGE_ID = "fruit_image_id";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fruit);
+        Intent intent = getIntent();
+        String fruitName = intent.getStringExtra(FRUIT_NAME);
+        int fruitImageId = intent.getIntExtra(FRUIT_IMAGE_ID, 0);
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        ImageView fruitImageView = (ImageView) findViewById(R.id.fruit_image_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView fruitContentText = (TextView) findViewById(R.id.fruit_content_text);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        collapsingToolbar.setTitle(fruitName);
+        Glide.with(this).load(fruitImageId).into(fruitImageView);
+        fruitContentText.setText(generateFruitContent(fruitName));
+    }
+
+    private String generateFruitContent(String fruitName) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 500; i++) {
+            builder.append(fruitName);
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- chpter12-materialdesigndemo/src/main/java/com/chpter12/materialdesign/demo/adapter/FruitAdapter.java
+++ chpter12-materialdesigndemo/src/main/java/com/chpter12/materialdesign/demo/adapter/FruitAdapter.java
@@ -1,6 +1,8 @@
 package com.chpter12.materialdesign.demo.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.chpter12.materialdesign.demo.FruitActivity;
 import com.chpter12.materialdesign.demo.R;
 import com.chpter12.materialdesign.demo.entity.Fruit;
 
@@ -32,7 +35,19 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder
             mContext = parent.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, parent, false);
-        return new MyViewHolder(view);
+        MyViewHolder holder = new MyViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+        return holder;
     }
 
     @Override
@@ -48,11 +63,13 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.MyViewHolder
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
         ImageView fruitImage;
         TextView fruitName;
 
         public MyViewHolder(View view) {
             super(view);
+            cardView = (CardView) view;
             fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
             fruitName = (TextView) view.findViewById(R.id.fruit_name);
         }
