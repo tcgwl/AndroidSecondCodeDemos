8356ffda1d092b4fa465ea9d7d8498505ba2cbf3
Archer <704617745@qq.com>
2017-10-25 11:15:25 +0800

    加入Toolbar。

---
+++ chpter12-materialdesigndemo/src/main/java/com/chpter12/materialdesign/demo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.chpter12.materialdesign.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toobar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup:
+                showToast("click backup");
+                break;
+            case R.id.delete:
+                showToast("click delete");
+                break;
+            case R.id.settings:
+                showToast("click settings");
+                break;
+        }
+        return true;
+    }
+
+    private Toast toast;
+
+    private void showToast(String msg) {
+        if (toast == null) {
+            toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(msg);
+        }
+        toast.show();
+    }
+}
