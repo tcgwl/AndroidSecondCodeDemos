b592ed38c538b186e598b40140be400b629da286
tcgwl <tcgwl_123@126.com>
2017-10-26 15:54:32 +0800

    完成下载Demo。

--- chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/MainActivity.java
+++ chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/MainActivity.java
@@ -1,13 +1,90 @@
 package com.chapter10.downloadservice.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.chapter10.downloadservice.demo.service.DownloadService;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button startDownloadBtn;
+    private Button pauseDownloadBtn;
+    private Button cancelDownloadBtn;
+
+    private DownloadService.DownloadBinder mDownloadBinder;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mDownloadBinder = (DownloadService.DownloadBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startDownloadBtn = (Button) findViewById(R.id.start_download_btn);
+        pauseDownloadBtn = (Button) findViewById(R.id.pause_download_btn);
+        cancelDownloadBtn = (Button) findViewById(R.id.cancel_download_btn);
+
+        startDownloadBtn.setOnClickListener(this);
+        pauseDownloadBtn.setOnClickListener(this);
+        cancelDownloadBtn.setOnClickListener(this);
+
+        Intent intent = new Intent(this, DownloadService.class);
+        startService(intent);
+        bindService(intent, mServiceConnection, BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mDownloadBinder == null)
+            return;
+        if (v == startDownloadBtn) {
+            String url = "http://download1.douding.cn/android/DocinReaderX6_3.8.2_DocIn.apk";
+//            String url = "https://raw.githubusercontent.com/guolindev/eclipse/master/eclipse-inst-win64.exe";
+            mDownloadBinder.startDownload(url);
+        } else if (v == pauseDownloadBtn) {
+            mDownloadBinder.pauseDownload();
+        } else if (v == cancelDownloadBtn) {
+            mDownloadBinder.cancelDownload();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
     }
 }
--- chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/service/DownloadService.java
+++ chapter10-downloadservicedemo/src/main/java/com/chapter10/downloadservice/demo/service/DownloadService.java
@@ -2,42 +2,60 @@ package com.chapter10.downloadservice.demo.service;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
 
+import com.chapter10.downloadservice.demo.MainActivity;
+import com.chapter10.downloadservice.demo.R;
 import com.chapter10.downloadservice.demo.interfaces.DownloadListener;
 import com.chapter10.downloadservice.demo.utils.DownloadTask;
 
+import java.io.File;
+
 public class DownloadService extends Service {
     private DownloadTask mDownloadTask;
-    private String downloadUrl;
+    private String mDownloadUrl;
     private DownloadListener mDownloadListener = new DownloadListener() {
         @Override
         public void onProgress(int progress) {
-
+            getNotificationManager().notify(1, getNotification("正在下载...", progress));
         }
 
         @Override
         public void onSuccess() {
-
+            mDownloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("下载成功", -1));
+            Toast.makeText(DownloadService.this, "下载成功", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onFailed() {
-
+            mDownloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("下载失败", -1));
+            Toast.makeText(DownloadService.this, "下载失败", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onPaused() {
-
+            mDownloadTask = null;
+            Toast.makeText(DownloadService.this, "暂停下载", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onCanceled() {
-
+            mDownloadTask = null;
+            stopForeground(true);
+            Toast.makeText(DownloadService.this, "取消下载", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -46,7 +64,18 @@ public class DownloadService extends Service {
     }
 
     private Notification getNotification(String title, int progress) {
-        return null;
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle(title);
+        if (progress > 0) {
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress, false);
+        }
+        return builder.build();
     }
 
     private DownloadBinder mBinder = new DownloadBinder();
@@ -56,15 +85,37 @@ public class DownloadService extends Service {
         return mBinder;
     }
 
-    class DownloadBinder extends Binder {
+    public class DownloadBinder extends Binder {
         public void startDownload(String url) {
-
+            if (mDownloadTask == null) {
+                mDownloadUrl = url;
+                mDownloadTask = new DownloadTask(mDownloadListener);
+                mDownloadTask.execute(url);
+                startForeground(1, getNotification("开始下载", 0));
+                Toast.makeText(DownloadService.this, "开始下载", Toast.LENGTH_SHORT).show();
+            }
         }
         public void pauseDownload() {
-
+            if (mDownloadTask != null) {
+                mDownloadTask.pauseDownload();
+            }
         }
         public void cancelDownload() {
-
+            if (mDownloadTask != null) {
+                mDownloadTask.cancelDownload();
+            } else {
+                if (mDownloadUrl != null) {
+                    String fileName = mDownloadUrl.substring(mDownloadUrl.lastIndexOf("/"));
+                    String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                    File file = new File(directory, fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    getNotificationManager().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownloadService.this, "取消下载", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 }
